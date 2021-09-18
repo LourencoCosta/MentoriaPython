7851360bc6a6c7136be14c251f222e9565983cd9
rafael.lourenco <rafael.lourenco@sensedia.com>
2021-09-17 23:38:24 -0300

    Melhoria do codigo no desafio avancado

--- desafios/desafio_1/desafio1_advanced.py
+++ desafios/desafio_1/desafio1_advanced.py
@@ -6,7 +6,7 @@ response = requests.request("GET", "https://viacep.com.br/ws/1305810/json/unicod
 if (response.status_code == 200):
     response_json = json.loads(response.text)
     string_template = "Logradouro: {}, Bairro: {}, Cidade: {}, Estado: {}, CEP: {}"
-    print(string_template.format(str(response_json['logradouro']), str(response_json['bairro']), str(response_json['localidade']), str(response_json['uf']), str(response_json['cep'])))
+    print(string_template.format(response_json['logradouro'], response_json['bairro'], response_json['localidade'], response_json['uf'], response_json['cep']))
 else:
     print ("Houve um erro ao consultar o cep")
 
